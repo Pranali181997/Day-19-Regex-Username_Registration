b5c119ac449c713e5e1c49ac6ecd2d3d4542e108
Pranali <pranalilambat00@gmail.com>
2022-03-03 16:13:04 +0530

    Regex_Username_Ragistration_Email

---
+++ Pass-9.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Day19_Regex
+{
+    class Pass_9
+    {
+        public static string REGEX_Email = "^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$";
+
+        public bool validEmail2(string Email)
+        {
+            return Regex.IsMatch(Email, REGEX_Email);
+        }
+    }
+}
---
+++ PassUC-7.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Day19_Regex
+{
+    class PassUC_7
+    {
+        public static string REGEX_Password = "^[A-Za-z0-9]{8,}$";
+
+        public bool validPasswordRule3(string Password)
+        {
+            return Regex.IsMatch(Password, REGEX_Password);
+        }
+    }
+}
