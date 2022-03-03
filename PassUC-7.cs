83722a9802581ca0827a1dd026dd341ed2611ade
Pranali <pranalilambat00@gmail.com>
2022-03-03 16:41:54 +0530

    UC-7_OneNum_Value

--- PassUC-7.cs
+++ PassUC-7.cs
@@ -5,11 +5,11 @@ using System.Text.RegularExpressions;
 
 namespace Day19_Regex
 {
-    class PassUC_6
+    class PassUC_7
     {
         public static string REGEX_Password = "^[A-Za-z0-9]{8,}$";
 
-        public bool validPasswordRule2(string Password)
+        public bool validPasswordRule3(string Password)
         {
             return Regex.IsMatch(Password, REGEX_Password);
         }
